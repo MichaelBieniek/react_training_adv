e9333733940b7431e2d8f684ddc96e2bab7000a0
Michael Bieniek <31993181+MichaelBieniek@users.noreply.github.com>
2018-06-05 07:50:32 -0400

    webpack

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+const config = {
+  entry: './lib/components/index.js',
+  output: {
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+			{ test: /\.js$/,
+				exclude: '/node_modules/',
+				use: {
+					loader: 'babel-loader',
+				} 
+			},
+    ],
+  },
+};
+
+module.exports = config;
